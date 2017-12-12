0cdee871872f63dd3532ec0d2eaaf98a2003f4c1
Kevin Hoyt <parkerkrhoyt@gmail.com>
2017-12-12 16:49:29 -0700

    Layer Hough on video feed.
    Move from static processing example, to live web camera video.

--- script/hough.js
+++ script/hough.js
@@ -1,27 +1,35 @@
 class Cube {
   constructor() {
-    console.log( 'Constructor.' );
+    // Data structures for analysis
+    this.lines = [];
 
     // References
+    this.camera = document.querySelector( 'video' );    
     this.canvas = document.querySelector( 'canvas' );
     this.context = this.canvas.getContext( '2d' );
 
-    // Load sample image
-    this.image = new Image( 640, 480 );
-    this.image.addEventListener( 'load', evt => this.doImageLoad( evt ) );
-    this.image.src = Cube.IMAGE_SOURCE;
+    // Access web camera
+    // Start detection
+    navigator.mediaDevices.getUserMedia( {audio: false, video: true} )
+      .then( ( stream ) => {
+        this.camera.srcObject = stream;
+        this.camera.onloadedmetadata = function( evt ) {
+          this.camera.play();
+          this.detect();
+        }.bind( this );
+      } ).catch( ( error ) => {
+        console.log( error );
+      } );
   }
 
   // Image processing
   detect() {
-    console.log( 'Detect.' );
-
     // Shortcuts
     let width = this.canvas.width;
     let height = this.canvas.height;
 
     // Put image onto canvas
-    this.context.drawImage( this.image, 0, 0, width, height );
+    this.context.drawImage( this.camera, 0, 0, width, height );
 
     // Get pixels
     // [red, green, blue, alpha, ...]
@@ -42,48 +50,45 @@ class Cube {
 
     // Canny
     jsfeat.imgproc.canny( image, image, 20, 40 );
-
-    // Hough
-    // Image, Rho, Theta, Threshold
-    let hough = jsfeat.imgproc.hough_transform( image, 1, Math.PI / 180, 115 );
-    console.log( hough );
     
     // Render
     this.render( image, raw );
 
+    // Hough
+    // Image, Rho, Theta, Threshold
+    this.lines = jsfeat.imgproc.hough_transform( image, 1, Math.PI / 180, 100 );
+
+    // Visualize
+    this.draw( 'green' );
+
+    // Continuous analysis
+    requestAnimationFrame( () => { return this.detect(); } );    
+  }
+
+  // Visualize analysis
+  draw( color, close = false ) {
     // Draw Hough lines
     // Ported from OpenCV documentation
-    for( let h = 0; h < hough.length; h++ ) {
-      let a = Math.cos( hough[h][1] );
-      let b = Math.sin( hough[h][1] );
-      let x0 = a * hough[h][0];
-      let y0 = b * hough[h][0];
+    for( let h = 0; h < this.lines.length; h++ ) {
+      let a = Math.cos( this.lines[h][1] );
+      let b = Math.sin( this.lines[h][1] );
+      let x0 = a * this.lines[h][0];
+      let y0 = b * this.lines[h][0];
       let x1 = x0 + this.canvas.width * ( 0 - b );
       let y1 = y0 + this.canvas.width * a;
       let x2 = x0 - this.canvas.width * ( 0 - b );
       let y2 = y0 - this.canvas.width * a;
 
-      console.log( hough[h][1] * ( 180 / Math.PI ) );
-      let degrees = hough[h][1] * ( 180 / Math.PI );
-
-      if( ( degrees > 87 && degrees < 93 ) ||
-          ( degrees > 177 && degrees < 180 ) || 
-          ( degrees > 0 && degrees < 3 ) ) {
-            this.context.beginPath();
-            this.context.moveTo( x1, y1 );
-            this.context.lineWidth = 2;
-            this.context.strokeStyle = 'green';
-            this.context.lineTo( x2, y2 );
-            this.context.stroke();
-      }
-
-
-    }
+      this.context.beginPath();
+      this.context.moveTo( x1, y1 );
+      this.context.lineWidth = 2;
+      this.context.strokeStyle = color;
+      this.context.lineTo( x2, y2 );
+      this.context.stroke();
+    }    
   }
 
   render( source, destination ) {
-    console.log( 'Render.' );
-
     // From matrix to image data
     let alpha = ( 0xff << 24 );
     let data = new Uint32Array( destination.data.buffer );    
@@ -98,18 +103,7 @@ class Cube {
     // Put back on canvas
     this.context.putImageData( destination, 0, 0 );
   }
-
-  // Image loaded
-  doImageLoad( evt ) {
-    console.log( 'Image loaded.' );
-
-    // Process image
-    this.detect();
-  }
 }
-
-// Constants
-Cube.IMAGE_SOURCE = 'img/rounded.cube.jpg';
 
 // Application
 let app = new Cube();
